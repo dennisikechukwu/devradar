4aaac78eaf1cbb813751a343960d9adfdfa9c499
Dennis <okwuchukwudennis001@gmail.com>
2025-06-09 18:42:19 +0100

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -54,7 +54,7 @@ const App: React.FC = () => {
       <p className="text-md md:text-xl mb-8 text-gray-300 text-center max-w-xl font-mono">
         Instantly search and explore GitHub developer profiles by username.
       </p>
-      <div className="flex w-full max-w-xs px-2 sm:max-w-md">
+      <div className="flex w-full max-w-xs  sm:max-w-md">
         <input
           type="text"
           value={username}
